c04912f22182ecefd685ea00da88974f4e698eb1
Damian Deugarte <39715792+dmdeugarte@users.noreply.github.com>
2024-01-05 12:19:20 -0600

    Introduce basic solving
    Removes unused input words
    Introduces a brute force method to find all solutions up to 3 words long (requires optimization)
    Separates initializing the valid words and finding a solution into separate methods.

--- src/Helper.java
+++ src/Helper.java
@@ -4,12 +4,25 @@ import java.util.Scanner;
 import java.io.FileWriter;
 import java.io.IOException;
 import java.lang.Math;
+import java.util.ArrayList;
 
 public class Helper
 {
   public static void main(String[] args)
   {
-    String myLetters = "BTLJWIEHYOCV";
+    String myLetters = "LACUDINSRTPW";
+    
+    doFirstPass(myLetters, true, true, true);
+    bruteForceSolution();
+  }
+  
+  /* Creates an output log, and reports as directed.
+  *  reportNum determines if the number of valid words will be reported to the console
+  *  reportLongest determines if the first, longest, valid word will be reported to the console
+  *  reportMostLetters determines if the first word that hits the most different letters will be reported to the console 
+  */
+  private static void doFirstPass(String myLetters, boolean reportNum, boolean reportLongest, boolean reportMostLetters) 
+  {
     String longest = "";
     String mostUsedLetters = "";
     int mostUsedLettersBitCount = 0;
@@ -19,18 +32,18 @@ public class Helper
       File input = new File("input.txt");
       Scanner reader = new Scanner(input);
       
-      File output = new File("output.txt");
+      File output = new File("validWords.txt");
       
       if (!output.createNewFile())
       {
-        System.out.println("Output file already exists.\nDeleting preexisting output file...");
+        //System.out.println("Output file already exists.\nDeleting preexisting output file...");
         output.delete();
-        System.out.println("Preexisting output file deleted.");
+        //System.out.println("Preexisting output file deleted.");
         output.createNewFile();
       }
-      System.out.println("Output file created.");
+      //System.out.println("Output file created.");
       
-      FileWriter writer = new FileWriter("output.txt");
+      FileWriter writer = new FileWriter("validWords.txt");
       
       int totalCount = 0;
       
@@ -47,6 +60,7 @@ public class Helper
         int mask = 0;
         boolean valid = true;
         
+        // check letter by letter
         while (i < length && valid)
         {
           int index = myLetters.indexOf(word.charAt(i));
@@ -66,20 +80,19 @@ public class Helper
             {
               valid = false;
             }
-            else
-            {
-              lastGroup = newGroup;
-            }
+            
+            lastGroup = newGroup; // If valid = false, saving this here won't matter. Less if-else logic
           }
           
           i++;
         }
         
+        // letters fully passed through, now add the new word to the reports
         if (valid)
         {
-          System.out.println(word + " is valid.");
+          //System.out.println(word + " is valid.");
           totalCount++;
-          writer.write(word + "\n");
+          writer.write(word + "," + mask + "\n");
           
           if (word.length() > longest.length())
             longest = word;
@@ -92,9 +105,20 @@ public class Helper
         }
       }
       
-      System.out.println(longest + " was the longest word.");
-      System.out.println(mostUsedLetters + " hit the most letters, with " + mostUsedLettersBitCount + " letters used.");
-      System.out.println(totalCount + " total valid words.");
+      // report checks
+      if (reportNum)
+      {
+        System.out.println(totalCount + " total valid words.");
+      }
+      if (reportLongest)
+      {
+        System.out.println(longest + " was the longest word.");
+      }
+      if (reportMostLetters)
+      {
+        System.out.println(mostUsedLetters + " hit the most letters, with " + mostUsedLettersBitCount + " letters used.");
+      }
+      
       reader.close();
       writer.close();
     } 
@@ -109,9 +133,120 @@ public class Helper
       e.printStackTrace();
     }
     
-    System.out.println("Program halt.");
+    System.out.println("First pass halt.");
   }
   
+  // using output.txt, finds the smallest length solutions
+  // requires optimization
+  // solutions put into arraylist: if word2;word3 is in solutions. 
+  // move tempMem into incompleteSolutions. 
+  // consolidate words and masks into one custom object type. 
+  public static void bruteForceSolution()
+  {
+    try
+    {
+      File input = new File("validWords.txt");
+      Scanner reader = new Scanner(input);
+      
+      File solutions = new File("solutions.txt");
+      
+      if (!solutions.createNewFile())
+      {
+        solutions.delete();
+        solutions.createNewFile();
+      }
+      
+      FileWriter solutionWriter = new FileWriter("solutions.txt");
+      
+      ArrayList<String> words = new ArrayList<String>();
+      ArrayList<Integer> masks = new ArrayList<Integer>();
+      
+      while (reader.hasNextLine())
+      {
+        String line = reader.nextLine(); // of pattern "word,mask"
+        int commaPosition = line.indexOf(',');
+        
+        String word = line.substring(0, commaPosition);
+        int mask = Integer.parseInt(line.substring(commaPosition+1));
+        
+        words.add(word);
+        masks.add(mask);
+      }
+      
+      ArrayList<String> tempWords = new ArrayList<String>();
+      ArrayList<Integer> tempMasks = new ArrayList<Integer>();
+      
+      for (int i = 0; i < words.size(); i++)
+      {
+        for (int j = 0; j < words.size(); j++)
+        {
+          String word1 = words.get(i);
+          String word2 = words.get(j);
+          
+          // last letter matches first letter
+          if (word1.charAt(word1.length()-1) == word2.charAt(0) && !word1.equals(word2))
+          {
+            String wordOut = word1 + ";" + word2;
+            int mask1 = masks.get(i);
+            int mask2 = masks.get(j);
+            
+            int mask = mask1 | mask2;
+            
+            if (countBits(mask) == 12)
+            {
+              solutionWriter.write(wordOut + "\n");
+            }
+            else
+            {
+              tempWords.add(wordOut);
+              tempMasks.add(mask);
+            }
+          }
+        }
+      }
+      
+      /*for (int i = 0; i < tempWords.size(); i++)
+      {
+        for (int j = 0; j < words.size(); j++)
+        {
+          String word1 = tempWords.get(i);
+          String word2 = words.get(j);
+          
+          // last letter matches first letter
+          if (word1.charAt(word1.length()-1) == word2.charAt(0) && !word1.equals(word2))
+          {
+            String wordOut = word1 + ";" + word2;
+            int mask1 = tempMasks.get(i);
+            int mask2 = masks.get(j);
+            
+            int mask = mask1 | mask2;
+            
+            if (countBits(mask) == 12)
+            {
+              solutionWriter.write(wordOut + "\n");
+            }
+          }
+        }
+      }*/
+      
+      reader.close();
+      solutionWriter.close();
+      
+      System.out.println("BruteForce Solutions found.");
+    } 
+    catch (FileNotFoundException e)
+    {
+      System.out.println("A file was not found.");
+      e.printStackTrace();
+    }
+    catch (IOException e)
+    {
+      System.out.println("An IO error occured.");
+      e.printStackTrace();
+    }
+  }
+  
+  // The naive flipped bit counter
   private static int countBits(int n) 
   {
     int count = 0;
